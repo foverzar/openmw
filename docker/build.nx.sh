e73703caa779373cdba6ed513b85698517acc97b
Vasily Pankratov <foverzar@gmail.com>
2024-11-11 17:31:50 +0300

    Prototype build env via docker

---
+++ docker/build.nx.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -xe
+
+# Creating build directory...
+mkdir -p build
+cd build
+
+# Running CMake..
+cmake \
+    -G"Unix Makefiles" \
+    -DSWITCH_LIBNX=ON \
+    -DCMAKE_CXX_FLAGS="-v -fpermissive -include cstdint -include limits" \
+    -DCMAKE_TOOLCHAIN_FILE="$DEVKITPRO/cmake/Switch.cmake" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPKG_CONFIG_EXECUTABLE="$PORTLIBS_PREFIX/bin/aarch64-none-elf-pkg-config" \
+    -DCMAKE_INSTALL_PREFIX="$PORTLIBS_PREFIX" \
+    -DMyGUI_LIBRARY="$PORTLIBS_PREFIX/lib/libMyGUIEngineStatic.a" \
+    -DBUILD_BSATOOL=OFF \
+    -DBUILD_NIFTEST=OFF \
+    -DBUILD_ESMTOOL=OFF \
+    -DBUILD_LAUNCHER=OFF \
+    -DBUILD_MWINIIMPORTER=OFF \
+    -DBUILD_ESSIMPORTER=OFF \
+    -DBUILD_OPENCS=OFF \
+    -DBUILD_WIZARD=OFF \
+    -DBUILD_MYGUI_PLUGIN=OFF \
+    -DOSG_STATIC=TRUE \
+    ..
+
+# Building with $NPROC CPU...
+make -j $NPROC
